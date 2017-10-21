d51419942219dfde5152fc91e65e616247a30751
Victor Ruch <viruch3112@gmail.com>
2017-10-21 18:29:03 +0200

    reformating cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -32,18 +32,14 @@
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing/render_face_detections.h>
 #include <dlib/image_processing.h>
-#include <dlib/gui_widgets.h>
 
 using namespace dlib;
 using namespace std;
 
-int main()
-{
-    try
-    {
+int main() {
+    try {
         cv::VideoCapture cap(0);
-        if (!cap.isOpened())
-        {
+        if (!cap.isOpened()) {
             cerr << "Unable to connect to camera" << endl;
             return 1;
         }
@@ -56,12 +52,10 @@ int main()
         deserialize("../res/shape_predictor_68_face_landmarks.dat") >> pose_model;
 
         // Grab and process frames until the main window is closed by the user.
-        while(!win.is_closed())
-        {
+        while (!win.is_closed()) {
             // Grab a frame
             cv::Mat temp;
-            if (!cap.read(temp))
-            {
+            if (!cap.read(temp)) {
                 break;
             }
             // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
@@ -85,15 +79,13 @@ int main()
             win.add_overlay(render_face_detections(shapes));
         }
     }
-    catch(serialization_error& e)
-    {
+    catch (serialization_error & e) {
         cout << "You need dlib's default face landmarking model file to run this example." << endl;
         cout << "You can get it from the following URL: " << endl;
         cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
         cout << endl << e.what() << endl;
     }
-    catch(exception& e)
-    {
+    catch (exception & e) {
         cout << e.what() << endl;
     }
 }
