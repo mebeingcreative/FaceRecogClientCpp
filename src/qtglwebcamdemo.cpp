72f69dce38a6bfb34d432f5f01a7598ef69bdaaa
Victor Ruch <viruch3112@gmail.com>
2017-11-07 11:26:52 +0100

    removing superflous try

--- src/qtglwebcamdemo.cpp
+++ src/qtglwebcamdemo.cpp
@@ -56,38 +56,30 @@ void QtGLWebcamDemo::timerEvent(QTimerEvent*) {
     cv::Mat image;
     capture.read(image);
 
-    try {
-            // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
-            // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
-            // long as temp is valid.  Also don't do anything to temp that would cause it
-            // to reallocate the memory which stores the image as that will make cimg
-            // contain dangling pointers.  This basically means you shouldn't modify temp
-            // while using cimg.
-            dlib::cv_image<dlib::bgr_pixel> cimg(image);
+    // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
+    // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
+    // long as temp is valid.  Also don't do anything to temp that would cause it
+    // to reallocate the memory which stores the image as that will make cimg
+    // contain dangling pointers.  This basically means you shouldn't modify temp
+    // while using cimg.
+    dlib::cv_image<dlib::bgr_pixel> cimg(image);
 
 
-            // Detect faces
-            std::vector<dlib::rectangle> faces = detector(cimg);
-            // Find the pose of each face.
-            std::vector<dlib::full_object_detection> shapes;
-            for (auto & face : faces) {
-                shapes.push_back(pose_model(cimg, face));
+    // Detect faces
+    std::vector<dlib::rectangle> faces = detector(cimg);
+    // Find the pose of each face.
+    std::vector<dlib::full_object_detection> shapes;
+    for (auto & face : faces) {
+        shapes.push_back(pose_model(cimg, face));
 //                cv::Mat face_mat;
 //                originalImage.copyTo(face_mat(dlibRectangleToOpenCV(face)));
 
-                grow_margin(face);
-            }
-
-            /*dlib::array<dlib::array2d<dlib::rgb_pixel>> face_chips;
-            extract_image_chips(cimg, get_face_chip_details(shapes), face_chips);
-            face_win.set_image(dlib::tile_images(face_chips));*/
-
-            ui->openCVviewer->showImage(image);
+        grow_margin(face);
     }
-    catch (dlib::serialization_error & e) {
-        qCritical() << "You need dlib's default face landmarking model file to run this example.\n"
-                "You can get it from the following URL: \n"
-                "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2\n\n"
-                  << e.what() << "\n";
-    }
+
+    /*dlib::array<dlib::array2d<dlib::rgb_pixel>> face_chips;
+    extract_image_chips(cimg, get_face_chip_details(shapes), face_chips);
+    face_win.set_image(dlib::tile_images(face_chips));*/
+
+    ui->openCVviewer->showImage(image);
 }
