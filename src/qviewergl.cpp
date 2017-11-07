9ff8d44a210cb801c3b2fc3925cef15224f7a977
Victor Ruch <viruch3112@gmail.com>
2017-11-07 11:06:22 +0100

    mirror camera

--- src/qviewergl.cpp
+++ src/qviewergl.cpp
@@ -9,13 +9,13 @@ QViewerGl::QViewerGl(QWidget* parent) :
 void QViewerGl::initializeGL() {
     float const vertices[] = {
             // Position      Texture
-            -1.0f, 1.0f, 0.0f, 0.0f, // Top-left
-            1.0f, 1.0f, 1.0f, 0.0f, // Top-right
-            1.0f, -1.0f, 1.0f, 1.0f, // Bottom-right
+            -1.0f, 1.0f, 1.0f, 0.0f, // Top-left
+            1.0f, 1.0f, 0.0f, 0.0f, // Top-right
+            1.0f, -1.0f, 0.0f, 1.0f, // Bottom-right
 
-            1.0f, -1.0f, 1.0f, 1.0f, // Bottom-right
-            -1.0f, -1.0f, 0.0f, 1.0f, // Bottom-left
-            -1.0f, 1.0f, 0.0f, 0.0f  // Top-left
+            1.0f, -1.0f, 0.0f, 1.0f, // Bottom-right
+            -1.0f, -1.0f, 1.0f, 1.0f, // Bottom-left
+            -1.0f, 1.0f, 1.0f, 0.0f  // Top-left
     };
 
     char const* vertexSource =
