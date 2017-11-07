89150c4b2ffd08eae77b10276b40f55876f6121c
Victor Ruch <viruch3112@gmail.com>
2017-11-07 11:05:29 +0100

    integrating dlib into library

--- src/qtglwebcamdemo.cpp
+++ src/qtglwebcamdemo.cpp
@@ -1,6 +1,28 @@
 #include <QDebug>
+
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
 #include "qtglwebcamdemo.h"
 #include "ui_qtglwebcamdemo.h"
+
+
+cv::Rect dlibRectangleToOpenCV(dlib::rectangle const & r) {
+    return {cv::Point2i(r.left(), r.top()), cv::Point2i(r.right() + 1, r.bottom() + 1)};
+}
+
+void convert_to_jpeg(cv::Mat & mat, std::vector<uchar> & out) {
+    std::vector<int> params{ cv::IMWRITE_JPEG_QUALITY, 80 };
+    cv::imencode(".jpg", mat, out, params);
+}
+
+template<int margins = 44L>
+void grow_margin(dlib::rectangle & r) {
+    r.left() -= margins;
+    r.top() -= margins;
+    r.right() += 2 * margins;
+    r.bottom() += 2 * margins;
+}
+
 
 QtGLWebcamDemo::QtGLWebcamDemo(QWidget* parent) :
         QMainWindow(parent) {
@@ -15,6 +37,18 @@ void QtGLWebcamDemo::on_actionStart_triggered() {
         qCritical() << "Failed to open camera";
         return;
     }
+
+    try {
+        detector = dlib::get_frontal_face_detector();
+        dlib::shape_predictor pose_model;
+        dlib::deserialize("../res/shape_predictor_68_face_landmarks.dat") >> pose_model;
+    } catch (dlib::serialization_error & e) {
+        qCritical() << "You need dlib's default face landmarking model file to run this example.\n"
+                "You can get it from the following URL: \n"
+                "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2\n\n"
+                  << e.what() << "\n";
+    }
+
     startTimer(0);
 }
 
@@ -22,7 +56,38 @@ void QtGLWebcamDemo::timerEvent(QTimerEvent*) {
     cv::Mat image;
     capture.read(image);
 
-    // Do what you want with the image :-)
+    try {
+            // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
+            // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
+            // long as temp is valid.  Also don't do anything to temp that would cause it
+            // to reallocate the memory which stores the image as that will make cimg
+            // contain dangling pointers.  This basically means you shouldn't modify temp
+            // while using cimg.
+            dlib::cv_image<dlib::bgr_pixel> cimg(image);
 
-    ui->openCVviewer->showImage(image);
+
+            // Detect faces
+            std::vector<dlib::rectangle> faces = detector(cimg);
+            // Find the pose of each face.
+            std::vector<dlib::full_object_detection> shapes;
+            for (auto & face : faces) {
+                shapes.push_back(pose_model(cimg, face));
+//                cv::Mat face_mat;
+//                originalImage.copyTo(face_mat(dlibRectangleToOpenCV(face)));
+
+                grow_margin(face);
+            }
+
+            /*dlib::array<dlib::array2d<dlib::rgb_pixel>> face_chips;
+            extract_image_chips(cimg, get_face_chip_details(shapes), face_chips);
+            face_win.set_image(dlib::tile_images(face_chips));*/
+
+            ui->openCVviewer->showImage(image);
+    }
+    catch (dlib::serialization_error & e) {
+        qCritical() << "You need dlib's default face landmarking model file to run this example.\n"
+                "You can get it from the following URL: \n"
+                "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2\n\n"
+                  << e.what() << "\n";
+    }
 }
--- src/qtglwebcamdemo.h
+++ src/qtglwebcamdemo.h
@@ -3,7 +3,14 @@
 
 #include <QMainWindow>
 #include <memory>
+#include <vector>
+
 #include <opencv2/highgui/highgui.hpp>
+#include <dlib/opencv.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+
 
 namespace Ui {
     class QtGLWebcamDemo;
@@ -14,17 +21,20 @@ Q_OBJECT
 
 public:
     explicit QtGLWebcamDemo(QWidget* parent = 0);
-    ~QtGLWebcamDemo();
+    ~QtGLWebcamDemo() override;
 
 protected:
-    void timerEvent(QTimerEvent* event);
+    void timerEvent(QTimerEvent* event) override;
 
 private slots:
     void on_actionStart_triggered();
 
 private:
     std::unique_ptr<Ui::QtGLWebcamDemo> ui;
-    cv::VideoCapture capture;
+    cv::VideoCapture capture{};
+    dlib::frontal_face_detector detector{};
+    dlib::shape_predictor pose_model{};
+    dlib::image_window face_win{};
 };
 
 #endif // QTGLWEBCAMDEMO_H
