72ed1b7156f84e2b636df38f923abcea5817b20f
Victor Ruch <viruch3112@gmail.com>
2017-11-15 00:14:55 +0100

    optimizing build

--- src/face_detect/face_detector.h
+++ src/face_detect/face_detector.h
@@ -7,7 +7,6 @@
 
 #include <dlib/opencv.h>
 #include <dlib/image_processing/frontal_face_detector.h>
-#include <dlib/image_processing/render_face_detections.h>
 #include <dlib/image_processing/shape_predictor.h>
 #include <QVector>
 #include <QRect>
