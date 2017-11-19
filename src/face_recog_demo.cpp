79c61ac1f0d9058f11ece78295b113e95a9dd5b6
Victor Ruch <viruch3112@gmail.com>
2017-11-19 11:00:07 +0100

    downgrading to webkit in order to support old ubuntu distros

--- src/face_recog_demo.cpp
+++ src/face_recog_demo.cpp
@@ -1,16 +1,17 @@
 #include <QDebug>
 
 #include "face_recog_demo.h"
-#include "ui_face_recog_demo.h"
 #include "face_recog_config.h"
+#include "ui_face_recog_demo.h"
 
 face_recog_demo::face_recog_demo(QWidget* parent):
         QMainWindow(parent){
     auto config = fetch_config();
     ui = std::make_unique<Ui::face_recog_demo>();
     ui->setupUi(this);
-    ui->webView->load(config.tracking_view_url);
-    ui->webView->show();
+    auto web_view = ui->webView;
+    web_view->load(config.tracking_view_url);
+    web_view->show();
 }
 
 face_recog_demo::~face_recog_demo() = default;
