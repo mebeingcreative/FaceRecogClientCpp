ae8f7a0749585a61770dc2f74f12765d054950b8
Victor Ruch <viruch3112@gmail.com>
2017-11-19 10:15:10 +0100

    correcting packaging

--- src/face_recog_config.h
+++ src/face_recog_config.h
@@ -6,41 +6,31 @@
 #define FACERECOGCLIENTCPP_CONFIG_H
 
 #include <string>
+#include <stdexcept>
 #include <QString>
 #include <QUrl>
 #include <QSettings>
-#include "auto_generated/face_recog_constants.h"
+#include <QFileInfo>
+#include <QDebug>
 
 struct face_recog_config{
-    face_recog_config(
-            QUrl const & embedding_host_url,
-            QUrl const & tracking_host_url,
-            QUrl const & tracking_view_url,
-            QString const & location_name,
-            std::string const & predictor_path
-    ):
-            embedding_host_url{embedding_host_url},
-            tracking_host_url{tracking_host_url},
-            tracking_view_url{tracking_view_url},
-            location_name{location_name},
-            predictor_path{predictor_path}
-    {}
-    QUrl const embedding_host_url;
-    QUrl const tracking_host_url;
-    QUrl const tracking_view_url;
-    QString const location_name;
-    std::string const predictor_path;
+    QUrl embedding_host_url;
+    QUrl tracking_host_url;
+    QUrl tracking_view_url;
+    QString location_name;
+    std::string predictor_path;
+    static QString path;
 };
 
 inline face_recog_config fetch_config(){
-    QSettings settings(FACE_RECOG_CONFIG_PATH, QSettings::IniFormat);
-    return {
-            QUrl{settings.value("embedding_host_url").toString()},
-            QUrl{settings.value("tracking_host_url").toString()},
-            QUrl{settings.value("tracking_view_url").toString()},
-            settings.value("location_name").toString(),
-            settings.value("predictor_path").toString().toStdString()
-    };
+    QSettings settings(face_recog_config::path, QSettings::IniFormat);
+    auto c = face_recog_config{};
+    c.embedding_host_url = QUrl{settings.value("embedding_host_url").toString()};
+    c.tracking_host_url = QUrl{settings.value("tracking_host_url").toString()};
+    c.tracking_view_url = QUrl{settings.value("tracking_view_url").toString()};
+    c.location_name = settings.value("location_name").toString();
+    c.predictor_path = settings.value("predictor_path").toString().toStdString();
+    return c;
 }
 
 #endif //FACERECOGCLIENTCPP_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,16 @@
 #include <QApplication>
 #include <QException>
 #include <QDebug>
-
+#include "face_recog_config.h"
 #include "face_recog_demo.h"
+
+QString face_recog_config::path = "./face-recog-config.ini";
 
 int main(int argc, char* argv[]) {
     int res = -1;
-
+    if (argc > 1){
+        face_recog_config::path = QString{argv[1]};
+    }
     try {
         QApplication a(argc, argv);
         face_recog_demo w{};
@@ -43,8 +47,8 @@ int main(int argc, char* argv[]) {
         res = a.exec();
     } catch (QException & e) {
         qCritical() << QString("Exception: %1").arg(e.what());
-    } catch (...) {
-        qCritical() << QString("Unhandled Exception");
+    } catch (std::exception & e) {
+        qCritical() << QString("Exception: %1").arg(e.what());
     }
 
     return res;
